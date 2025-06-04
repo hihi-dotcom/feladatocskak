4f4c4014be9b60323c8f839aa755c3590259c886
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-06-04 22:07:03 +0200

    működik a sima rendezes is

--- rendezes/scripts_1.js
+++ rendezes/scripts_1.js
@@ -1,9 +1,8 @@
 class DataToTable{
-    tomb;
-
-    constructor(at_tomb){
-        this.tomb = at_tomb;
-
+ 
+   #area;
+    constructor(area){
+        this.#area = area;
         const table = this.htmlElementMaker(`table`,``, document.body);
         const thead = this.htmlElementMaker(`thead`,``, table);
         const tbody = this.htmlElementMaker(`tbody`,``, table);
@@ -13,15 +12,33 @@ class DataToTable{
         this.htmlElementMaker(`th`, `Hónap`, thead);
         this.htmlElementMaker(`th`, `Összeg`, thead)
 
-        for(const element of at_tomb){
+        this.#area.settingTableCallback((tomb) => {
+            for(const element of tomb){
+           
             const tr = this.htmlElementMaker(`tr`, ``, tbody);
             this.htmlElementMaker(`td`, element.megnevezes, tr);
             this.htmlElementMaker(`td`, element.helyszin, tr);
             this.htmlElementMaker(`td`, element.honap, tr);
-         const koltseg = this.htmlElementMaker(`td`, element.koltseg, tr);
+            const koltseg = this.htmlElementMaker(`td`, element.koltseg, tr);
          this.renderingmoney(element, koltseg);
         }
+        });
+
+        this.#area.settingRenderCallback((rendezett_tomb) => {
+            tbody.innerHTML = '';
+            for(const elem of rendezett_tomb){
+                const tr_rendezett = this.htmlElementMaker('tr', '', tbody);
+                this.htmlElementMaker('td', elem.megnevezes, tr_rendezett);
+                this.htmlElementMaker('td', elem.helyszin, tr_rendezett);
+                this.htmlElementMaker('td', elem.honap, tr_rendezett);
+                const koltseges = this.htmlElementMaker('td', elem.koltseg, tr_rendezett);
+                this.renderingmoney(elem, koltseges);
+            }
+        });
+
     };
+
+
 
     htmlElementMaker(htmlelemtype, innerhtml, parent){
         const htmlelement = document.createElement(htmlelemtype);
@@ -50,7 +67,23 @@ class Area{
 
     valuesarray;
     csokkenovagynovekvoarray;
+     #RenderCallback;
+     #fajltomb;
+     #TableCallback;
+
+     get fajltomb(){
+        return this.#fajltomb;
+     }
+
+    settingRenderCallback(bem_callback){
+        this.#RenderCallback = bem_callback;
+    }
+    settingTableCallback(bem1_callback){
+        this.#TableCallback = bem1_callback;
+    }
     constructor(){
+        this.#fajltomb = [];
+        this.Downloading();
      const form = document.createElement("form");
      document.body.appendChild(form);
 
@@ -98,11 +131,33 @@ class Area{
         submitgomb.innerHTML = "Rendezés";
         submitgomb.type = "submit";
 
-        submitgomb.addEventListener(`submit`, (e) => {
+        form.addEventListener(`submit`, (e) => {
             e.preventDefault();
-            for(const kolt_bev of this.tomb){
-                this.selectkivalasztas(select_elem, 'Hónap',)
-            }
+
+            this.orderBy((koltseg1, koltseg2) => {
+                let ertek_tarolo = false;
+                let value_1 = koltseg1[select_elem.value];
+                let value_2 = koltseg2[select_elem.value];
+                if(select_elem.value !== "koltseg"){
+                    if(csokkenovagynovekvo.value === "csokkeno" && value_1.localeCompare(value_2) < 0){
+                        ertek_tarolo = true;
+                    }
+                    else if(csokkenovagynovekvo.value === "novekvo" && value_1.localeCompare(value_2) > 0){
+                        ertek_tarolo = true;
+                    }
+                }
+                else if(select_elem.value == "koltseg" && csokkenovagynovekvo.value == "csokkeno"){
+                        if(value_1 < value_2){
+                            ertek_tarolo = true;
+                        }
+                }
+                else if(select_elem.value == "koltseg" && csokkenovagynovekvo.value == "novekvo"){
+                    if(value_1 > value_2){
+                        ertek_tarolo = true;
+                    }
+                }
+                return ertek_tarolo;
+            });
             
 
         });
@@ -125,67 +180,63 @@ class Area{
     orderBy(rendezo_callback){
         const result_rendezes = [];
 
-        for(const koltseg of this.tomb){
+        for(const koltseg of this.#fajltomb){
             result_rendezes.push(koltseg);
         }
         for(let a = 0; a < result_rendezes.length; a++){
             
             for(let j = a + 1; j < result_rendezes.length; j++){
-                if(rendezo_callback(result_rendezes[i], result_rendezes[j])){
-                    const temp = result_rendezes[i];
-                    result_rendezes[i] = result_rendezes[j];
+                if(rendezo_callback(result_rendezes[a], result_rendezes[j])){
+                    const temp = result_rendezes[a];
+                    result_rendezes[a] = result_rendezes[j];
                     result_rendezes[j] = temp;
                 };
             };
             
         };
+       this.#RenderCallback(result_rendezes);
+    };
+    Downloading(){
+                const input_file = document.createElement(`input`);
+        input_file.type = `file`;
+        document.body.appendChild(input_file);
+
+        input_file.addEventListener(`change`, (e) => {
+            const sajatfile = e.target.files[0];
+
+            const fajlbeolvaso = new FileReader();
+
+            fajlbeolvaso.readAsText(sajatfile);
+            fajlbeolvaso.onload = (e) => {
+                const sajat_fajltart = fajlbeolvaso.result;
+                const sajatfajlunksorai = sajat_fajltart.split('\n');
+                const kolt_bevetel_tomb = [];
+                for(const sor of sajatfajlunksorai){
+                    const trimmer = sor.trim();
+                    if(trimmer !== ''){
+                        const sorelemek = sor.split(';');
+                        const kolt_bevetel= {
+                            megnevezes: sorelemek[0],
+                            helyszin: sorelemek[1],
+                            honap: sorelemek[2],
+                            koltseg: parseInt(sorelemek[3]),
+                        }
+                        this.#fajltomb.push(kolt_bevetel);
+                        
+                    }
+
+                };
+                this.#TableCallback(this.#fajltomb);
+            };
+        });
     };
 
-/**
- * 
- * @param {HTMLElement} selectrendezo 
- * @param {string} vizsgalnikivantmegnevezes 
- * @param {function(koltseg, koltseg):boolean} rendezofuggveny 
- */
-    selectkivalasztas(selectrendezo, vizsgalnikivantmegnevezes, property1,){
-        if(selectrendezo.value === vizsgalnikivantmegnevezes){
-            this.orderBy((koltseg1, koltseg2) => {
-                return koltseg1.property1.localCompare(koltseg2.property1)
-            });
-        };
-    }
+
 };
 
 
 
-const input_file = document.createElement(`input`);
-input_file.type = `file`;
-document.body.appendChild(input_file);
 
-input_file.addEventListener(`change`, (e) => {
-    const sajatfile = e.target.files[0];
-
-    const fajlbeolvaso = new FileReader();
-
-    fajlbeolvaso.readAsText(sajatfile);
-    fajlbeolvaso.onload = (e) => {
-        const sajat_fajltart = fajlbeolvaso.result;
-        const sajatfajlunksorai = sajat_fajltart.split('\n');
-        const kolt_bevetel_tomb = [];
-        for(const sor of sajatfajlunksorai){
-            const sorelemek = sor.split(';');
-            const kolt_bevetel= {
-                megnevezes: sorelemek[0],
-                helyszin: sorelemek[1],
-                honap: sorelemek[2],
-                koltseg: sorelemek[3],
-            }
-            kolt_bevetel_tomb.push(kolt_bevetel);
-            
-        };
-        const adattablazat = new DataToTable(kolt_bevetel_tomb);
-        console.log(kolt_bevetel_tomb);
-    };
-});
 
 const arena = new Area();
+const adat_to_t = new DataToTable(arena);
